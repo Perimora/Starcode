743576f07759828876578a8b66720c9093ee6c7c
johanna-hennecke <j.hennecke@campus.tu-berlin.de>
2024-12-28 17:47:12 +0100

    kleinere Anpassungen

--- hangman_Minh_Anh.py
+++ hangman_Minh_Anh.py
@@ -9,7 +9,8 @@ def verringere_um_Eins(zahl):  # grundlegende Funktion
     return zahl - 1
 
 
-def Begruessung_und_Eroeffnung():  # Nutzung bereits gegebener Funktionen wie input
+def kündige_Spieleröffnung_an():
+# Nutzung bereits gegebener Funktionen wie input
     print("Willkommen beim Hangman-Spiel")
     name = input("Bitte gib deinen Namen ein: ")
     print("Hallo " + name + "! Schön dich kennenzulernen.")
@@ -19,23 +20,24 @@ def Begruessung_und_Eroeffnung():  # Nutzung bereits gegebener Funktionen wie in
 
 def ist_das_Spiel_noch_am_laufen(erratenesTeilwort, erlaubteFehlversuche):  # if else Funktion
     if (not ("_" in erratenesTeilwort)):
-        print("GAME OVER - YOU WON")
+        print("CONGRATULATIONS! - YOU WON")
         return False
 
     if (erlaubteFehlversuche == 0):
         print("GAME OVER - YOU LOST")
         return False
 
-    return True
+    else:
+        return True
 
 
-def erhalte_Großbuchstaben():  # While Funktion
+def erhalte_Großbuchstaben():   
     eingabebuchstabe = input("Gib einen Großbuchstaben ein: ")
-    while (not (len(eingabebuchstabe) == 1 and eingabebuchstabe.isalpha() and eingabebuchstabe.isupper())):
-        print("Das war kein Großbuchstabe: ")
+    while (not (len(eingabebuchstabe) == 1 and eingabebuchstabe.isalpha())):
+        print("Das war kein einzelner Buchstabe: ")
         eingabebuchstabe = input("Gib einen Großbuchstaben ein: ")
 
-    return eingabebuchstabe
+    return eingabebuchstabe.upper()
 
 
 # TODO: Hier ne eigene Funktion zu machen bietet iwie keinen Mehrwert.
@@ -50,15 +52,13 @@ def gib_verschleiertes_Wort(geheimwort):  # For Funktion
     verschleiertesWort = []
     for buchstabe in geheimwort:
         verschleiertesWort.append("_")
-        # TODO: Wieso hier Spacing?
-        verschleiertesWort.append(" ")
 
     return verschleiertesWort
 
 
 # TODO: Die Funktion ist cool! Ich würde die als API für die GUI nutzen und die dann ggf. nochmal refactoren...
 def zeige_Zustand(erratenesTeilwort, fehlgeschlageneBuchstaben, anzahlDerFehlversuche):  # Listen Funktion 3
-    print("Das bisher erratene Teilwort ist: " + ''.join(erratenesTeilwort))
+    print("Das bisher erratene Teilwort ist: " + ' '.join(erratenesTeilwort))
     print("Die fehlgeschlagenen Buchstaben sind: " + str(fehlgeschlageneBuchstaben))
     print("Du hast noch " + str(anzahlDerFehlversuche) + " Fehlversuche übrig.")
     print("-------------------------------------------------")
@@ -77,7 +77,7 @@ def wähle_ein_zufälliges_Wort(wörterliste):  # Listen Funktion 4
 def entschleiere_Buchstaben(eingabebuchstabe, geheimwort, erratenesTeilwort):  # For
     for buchstabenposition, buchstabe in enumerate(geheimwort):
         if (eingabebuchstabe == buchstabe):
-            erratenesTeilwort[buchstabenposition * 2] = eingabebuchstabe
+            erratenesTeilwort[buchstabenposition] = eingabebuchstabe
 
 
 # Eigentliches Spiel

3985772810c3b4c712eb9998be9558426430c07e
Christoph Huy <christoph.huy@campus.tu-berlin.de>
2025-01-03 11:00:28 +0100

    refactor file structure

--- lib/hangman.py
+++ lib/hangman.py
@@ -1,122 +1,114 @@
 import random
+from time import sleep
+
+from lib.hangman_gui import HangmanGui
 
 
 # TODO: Wie unterer Kommentar beim Kapseln der Built-In Funktion.
 #       Weiß nicht, ob es hier sinnvoll ist zu kapseln, wenn das eigentlich eine 1 Zeilen Operation ist...
 
 # gekapselte Funktionen
-def verringere_um_Eins(zahl):  # grundlegende Funktion
-    zahl = zahl - 1
-
-    return zahl
+def verringere_um_eins(zahl):  # grundlegende Funktion
+    return zahl - 1
 
 
-def kündige_Spieleröffnung_an():  # Nutzung bereits gegebener Funktionen wie input
+def kündige_spieleröffnung_an():
+# Nutzung bereits gegebener Funktionen wie input
     print("Willkommen beim Hangman-Spiel")
     name = input("Bitte gib deinen Namen ein: ")
     print("Hallo " + name + "! Schön dich kennenzulernen.")
     print("-------------------------------------------------")
-    print("Let's Play Hangman! Du kannst dir 6 Fehlversuche leisten.")
+    print("Let's Play Hangman! Du kannst dir 10 Fehlversuche leisten.")
 
 
-def ist_das_Spiel_noch_am_laufen(erratenesTeilwort, erlaubteFehlversuche):  # if else Funktion
-    if (not ("_" in erratenesTeilwort)):
-        print("GAME OVER - YOU WON")
+def ist_das_spiel_noch_am_laufen(erratenes_teilwort, erlaubte_fehlversuche):  # if else Funktion
+    if (not ("_" in erratenes_teilwort)):
+        print("CONGRATULATIONS! - YOU WON")
         return False
 
-    if (erlaubteFehlversuche == 0):
+    if (erlaubte_fehlversuche == 0):
         print("GAME OVER - YOU LOST")
         return False
 
-    return True
+    else:
+        return True
 
-
-def erhalte_Großbuchstaben():  # While Funktion
+#
+def erhalte_großbuchstaben():
     eingabebuchstabe = input("Gib einen Großbuchstaben ein: ")
-    while (not (len(eingabebuchstabe) == 1 and eingabebuchstabe.isalpha() and eingabebuchstabe.isupper())):
-        print("Das war kein Großbuchstabe: ")
+    while (not (len(eingabebuchstabe) == 1 and eingabebuchstabe.isalpha())):
+        print("Das war kein einzelner Buchstabe: ")
         eingabebuchstabe = input("Gib einen Großbuchstaben ein: ")
 
-    return eingabebuchstabe
+    return eingabebuchstabe.upper()
 
 
 # TODO: Hier ne eigene Funktion zu machen bietet iwie keinen Mehrwert.
 #       Ich denke, dass verwirrt eher und bläht den Code auf.
-def erweitere_fehlgeschlagene_BuchstabenListe(eingabebuchstabe, fehlgeschlageneBuchstaben):  # Listen Funktion 1
-    fehlgeschlageneBuchstaben.append(eingabebuchstabe)
+def erweitere_fehlgeschlagene_BuchstabenListe(eingabebuchstabe, fehlgeschlagene_buchstaben):  # Listen Funktion 1
+    fehlgeschlagene_buchstaben.append(eingabebuchstabe)
 
 
 # Spacing entfernen?
 # Liste-Konzept vor for-Konzept
-def gib_verschleiertes_Wort(geheimwort):  # For Funktion
+def gib_verdecktes_wort(geheimwort):  # For Funktion
     verschleiertesWort = []
     for buchstabe in geheimwort:
         verschleiertesWort.append("_")
-        # TODO: Wieso hier Spacing?
-        verschleiertesWort.append(" ")
 
     return verschleiertesWort
 
-
-# TODO: Die Funktion ist cool! Ich würde die als API für die GUI nutzen und die dann ggf. nochmal refactoren...
-def zeige_Zustand(erratenesTeilwort, fehlgeschlageneBuchstaben, anzahlDerFehlversuche):  # Listen Funktion 3
-    print("Das bisher erratene Teilwort ist: " + ''.join(erratenesTeilwort))
-    print("Die fehlgeschlagenen Buchstaben sind: " + str(fehlgeschlageneBuchstaben))
-    print("Du hast noch " + str(anzahlDerFehlversuche) + " Fehlversuche übrig.")
-    print("-------------------------------------------------")
-
-
 # im Spielcode vorgeben
-def wähle_ein_zufälliges_Wort(wörterliste):  # Listen Funktion 4
-    wort = random.choice(wörterliste)
-
-    return wort
+def wähle_ein_zufälliges_wort(wörter_liste):  # Listen Funktion 4
+    wort = random.choice(wörter_liste)
+    # TODO: for unified representation i changed that here.
+    #   I'm considering to force upper case for user inputs as well to un-bloat the code for the students...
+    return wort.upper()
 
 
 # for i in range ?
 # Buchstabenposition * 2 -> Buchstaben ?
 # -> einfache Funktion
-def entschleiere_Buchstaben(eingabebuchstabe, geheimwort, erratenesTeilwort):  # For
+def buchstaben_aufdecken(eingabebuchstabe, geheimwort, erratenes_teilwort):  # For
     for buchstabenposition, buchstabe in enumerate(geheimwort):
         if (eingabebuchstabe == buchstabe):
-            erratenesTeilwort[buchstabenposition * 2] = eingabebuchstabe
+            erratenes_teilwort[buchstabenposition] = eingabebuchstabe
 
 
 # Eigentliches Spiel
-def starte_Hangmanspiel(wörterListe):
-    kündige_Spieleröffnung_an()
+def starte_hangmanspiel(wörter_liste):
 
-    geheimwort = wähle_ein_zufälliges_Wort(wörterListe)
+    kündige_spieleröffnung_an()
+    geheimwort = wähle_ein_zufälliges_wort(wörter_liste)
     fehlgeschlageneBuchstaben = []
-    erratenesTeilwort = gib_verschleiertes_Wort(geheimwort)
-    erlaubteFehlversuche = 6
+    erratenesTeilwort = gib_verdecktes_wort(geheimwort)
+    erlaubteFehlversuche = 10
 
-    zeige_Zustand(erratenesTeilwort, fehlgeschlageneBuchstaben, erlaubteFehlversuche)
+    sleep(0.5)
+
+    gui = HangmanGui(geheimwort, erratenesTeilwort, fehlgeschlageneBuchstaben, erlaubteFehlversuche)
+
+    #zeige_Zustand(erratenesTeilwort, fehlgeschlageneBuchstaben, erlaubteFehlversuche)
 
     # TODO: Vielleicht würde hier ein einfacher Boolean Abgleich leichter zu verstehen sein.
     #       Weiß nicht, ob das für den Anfang zu verkapselt ist.
-    while (ist_das_Spiel_noch_am_laufen(erratenesTeilwort, erlaubteFehlversuche)):
-        eingabebuchstabe = erhalte_Großbuchstaben()
+    while (ist_das_spiel_noch_am_laufen(erratenesTeilwort, erlaubteFehlversuche)):
+        eingabebuchstabe = erhalte_großbuchstaben()
 
         if (eingabebuchstabe in erratenesTeilwort):
-            print("Der Buchstabe ist bereits erraten. Versuche einen anderen Buchstaben.")
-            continue
+            nachricht ="Der Buchstabe ist bereits erraten.\nVersuche einen anderen Buchstaben."
 
         if (eingabebuchstabe in geheimwort):
-            print("> Glückwunsch, der Buchstabe ist im Wort enthalten")
-            entschleiere_Buchstaben(eingabebuchstabe, geheimwort, erratenesTeilwort)
+            nachricht = "Glückwunsch, der Buchstabe ist im Wort enthalten"
+            buchstaben_aufdecken(eingabebuchstabe, geheimwort, erratenesTeilwort)
 
         if (eingabebuchstabe in fehlgeschlageneBuchstaben):
-            print("Der Buchstabe ist bereits fehlgeschlagenen. Versuche einen anderen Buchstaben.")
-            continue
+            nachricht="Der Buchstabe ist bereits fehlgeschlagenen.\nVersuche einen anderen Buchstaben!"
 
         if (not (eingabebuchstabe in geheimwort)):
-            print("> Leider ist der Buchstabe nicht im Wort enthalten. Du hast nun einen Fehlversuch weniger.")
-            erlaubteFehlversuche = verringere_um_Eins(erlaubteFehlversuche)
+            erlaubteFehlversuche = verringere_um_eins(erlaubteFehlversuche)
             erweitere_fehlgeschlagene_BuchstabenListe(eingabebuchstabe, fehlgeschlageneBuchstaben)
+            nachricht="Leider ist der Buchstabe nicht im Wort enthalten.\nDu hast nun einen Fehlversuch weniger."
 
-        zeige_Zustand(erratenesTeilwort, fehlgeschlageneBuchstaben, erlaubteFehlversuche)
+        gui.cycle(erratenesTeilwort, erlaubteFehlversuche, fehlgeschlageneBuchstaben, message=nachricht)
 
-
-if __name__ == "__main__":
-    starte_Hangmanspiel(["HALLO"])
